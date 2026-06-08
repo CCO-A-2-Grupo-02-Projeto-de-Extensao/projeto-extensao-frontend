7799563174cff22c2825c6c6fc7124ede2eb941c
Pedro Abramo <pedro.dalmeida@sptech.school>
2026-06-08 17:27:33 -0300

    Feat: Adicionando VLibras no projeto - Acessibilidade

--- projeto-tamoios/src/main.jsx
+++ projeto-tamoios/src/main.jsx
@@ -5,6 +5,21 @@ import App from "./App.jsx";
 import "@fontsource/fredoka";
 import "@fontsource/fredoka/500.css";
 
+// Adicionando o VLibras
+const initVLibras = () => {
+  if (window.VLibras) {
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+    return;
+  }
+  const s = document.createElement('script');
+  s.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+  s.async = true;
+  s.onload = () => new window.VLibras.Widget('https://vlibras.gov.br/app');
+  document.body.appendChild(s);
+};
+
+initVLibras();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
