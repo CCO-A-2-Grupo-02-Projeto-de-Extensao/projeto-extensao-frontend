0671a069601e1c3127f5d607abb07d856647f41d
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2026-06-08 20:22:33 -0300

    fix: reajuste caminho com URL

--- projeto-tamoios/src/components/ImagemPublic/ImagemPublic.jsx
+++ projeto-tamoios/src/components/ImagemPublic/ImagemPublic.jsx
@@ -1,12 +1,15 @@
 import styles from "../../styles/imagem-public.module.css";
 
 export function ImagemPublic({ nomeImagem }) {
+  const imgUrl = new URL(`../../assets/${nomeImagem}`, import.meta.url).href;
+
   return (
     <img
       className={styles.imagemPublic}
-      src={`/${nomeImagem}`}
+      src={imgUrl}
       alt={`Imagem ${nomeImagem}`}
     />
   );
 }
+
 export default ImagemPublic;
